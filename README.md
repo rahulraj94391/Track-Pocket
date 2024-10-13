f143a7fa7a7d3402bfcf111f1a13e8e243c1532a
rahul-18968 <rahulraj94391@gmail.com>
2024-10-13 16:02:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Track Pocket: Personal Finance Tracker
---
+++ src/main/java/com/org/trackpocket/TrackPocketApplication.java
@@ -0,0 +1,13 @@
+package com.org.trackpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackPocketApplication.class, args);
+    }
+
+}
